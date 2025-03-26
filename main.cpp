933365ce0c2fcc9fed8d3424edd06fe48a1b6944
Flores-Ryan <superskier639@gmail.com>
2025-03-26 20:52:55 +0000

    finished

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int add(int a, int b);
+
+int main()
+{
+    cout<< "hello world\n";
+    return 0;
+
+
+
+}
+int add(int a, int b)
+{
+
+    
+return a+b;
+}
