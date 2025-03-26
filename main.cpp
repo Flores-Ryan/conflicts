f1b357bf109f1b773d53279a87c5d56dee8a391d
Ryan Flores <rtflores@students.nic.edu>
2025-03-26 13:51:33 -0700

    done

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
+void say(string s);
 int main()
 {
     cout<< "I like big Butts\n";
     cout<< "Hello world\n";
-
+    void say();  
     return 0;
-
+      
 
 
 }
+void say(string s)
+    {
+
+        cout << s << endl;        
+    }
